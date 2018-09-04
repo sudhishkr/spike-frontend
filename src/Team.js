1e17a0bf9b989a35225a0885859c33ab9343f6ed
sudhish kasaba ramesh <sudhishr@wepay.com>
2018-09-04 09:21:46 -0700

    fix Team sub heading

--- src/Team.js
+++ src/Team.js
@@ -8,7 +8,7 @@ class Team extends Component {
                     <div class="row">
                         <div class="col-lg-12 text-center">
                             <h2 class="section-heading text-uppercase">Our Team</h2>
-                            <h3 class="section-subheading text-muted">we are believe in lean growth</h3>
+                            <h3 class="section-subheading text-muted"></h3>
                         </div>
                     </div>
                     <div class="row">
